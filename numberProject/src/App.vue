2778fb5cebc5165442724ed97e77ed633d7f8418
farida04 <f.oumatous@gmail.com>
2018-02-20 16:51:00 +0100

    'test'

--- numberProject/src/App.vue
+++ numberProject/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/menu.svg">
+    <hr id="hr">
     <router-view/>
   </div>
 </template>
@@ -11,13 +12,17 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+img{
+    height: 10vh;
+    width: 10vw;
+    position: absolute;
+    right: 10px;
+}
+#hr{
+position: relative;
+top: 80px;
+height: 5px;
+background: #44A5A5;
 }
 </style>
---
+++ numberProject/src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  // data () {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App'
+  //   }
+  // }
+}
+</script>
+
+
+<style scoped>
+
+</style>
