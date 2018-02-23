c8eab3b9e6971b2d163bf7f386a7a1e25fbb944d
Richard Phonthibsvads <phs.richards@gmail.com>
2018-02-23 10:54:23 +0100

    ajout de la vue inscription

---
+++ numberProject/src/components/inscription.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="inscription">
+      <h1>Créer un compte</h1>
+
+      <form>
+          <input class='connect' type="text" name="pseudo" placeholder="pseudo" required>
+          <input class='connect' type="text" name="pseudo" placeholder="adresse mail" required>
+          <input class='connect' type="password" name="password" placeholder="mot de passe" required>
+          <input class='connect' type="password" name="password" placeholder="confirmer mot de passe" required>
+          <router-link to='/menu'>
+              <input id='btn' type="submit" name="inscription" placeholder="inscription">
+         </router-link>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Inscription',
+}
+</script>
+
+
+<style scoped>
+
+</style>
