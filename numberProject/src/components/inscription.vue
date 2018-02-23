f51999a018d0bf24ff1cc0ffad4128ba9874651b
Richard Phonthibsvads <phs.richards@gmail.com>
2018-02-23 12:00:30 +0100

    ajout style vue inscription

--- numberProject/src/components/inscription.vue
+++ numberProject/src/components/inscription.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="inscription">
+      <img id="logo" src="./../assets/logo.svg" alt="logo number">
       <h1>Créer un compte</h1>
 
       <form>
@@ -23,4 +24,53 @@ export default {
 
 <style scoped>
 
+.inscription {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+
+#logo{
+    width: 300px;
+    height: 300px;
+    position: absolute;
+    top: 30px;
+    left: 80px;
+}
+
+form{
+    position: absolute;
+    top:300px;
+    left: 40px;
+}
+
+.connect{
+     display: block;
+     border: none;
+     margin: 30px auto;
+     width: 70vw;
+     border-bottom: 3px solid #44A5A5;
+     text-align: center;
+     font-size: 1.8em;
+ }
+
+ #btn{
+     border: none;
+     background: none;
+     border: 3px solid #E41D1A;
+     width: 300px;
+     height: 80px;
+     background: #44A5A5;
+     color: #fff;
+     margin: 20px 40px;
+     font-size: 1.8em;
+ }
+ #new{
+     position: absolute;
+     top: 580px;
+     left: 110px;
+     font-size: 1.8em;
+     color: dimGray;
+     text-decoration: underline;
+ }
 </style>
